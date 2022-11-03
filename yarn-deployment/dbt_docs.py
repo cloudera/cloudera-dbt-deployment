a7beba756b61025537f401fb435a77060626d77f
Tapas Kumar Senapati <tksenapa@ncsu.edu>
2022-11-04 02:50:24 +0530

    Parameterise dbt project name and path

--- yarn-deployment/dbt_docs.py
+++ yarn-deployment/dbt_docs.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 # Copyright 2022 Cloudera Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -42,6 +41,8 @@ def load_fetch_environment_variables():
     ENV_VARIABLES["yarn_principal"] = os.getenv("YARN_PRINCIPAL")
     ENV_VARIABLES["yarn_keytab"] = os.getenv("YARN_KEYTAB")
     ENV_VARIABLES["yarn_rm_uri"] = os.getenv("YARN_RM_URI")
+    ENV_VARIABLES["git_project_url"] = os.getenv("GIT_PROJECT_URL")
+    ENV_VARIABLES["dbt_project_path"] = os.getenv("DBT_PROJECT_PATH")
 
 
 # generate JSON payload dynamically to send to yarn container to generate /serve dbt docs
@@ -56,10 +57,18 @@ def generate_yarn_payload():
     component["number_of_containers"] = 1
     component[
         "launch_command"
-    ] = "python3 -m venv /tmp/hive-dbt && source /tmp/hive-dbt/bin/activate && pip install dbt-hive dbt-impala && git clone https://github.com/cloudera/dbt-hive-example.git && cd dbt-hive-example/dbt_hive_demo && dbt docs generate --profiles-dir={} && python3 -m http.server 8888 --directory target".format(
+    ] = "python3 -m venv /tmp/dbt-yarn && source /tmp/dbt-yarn/bin/activate && pip install dbt-hive dbt-impala && git clone $GIT_PROJECT_URL && cd $DBT_PROJECT_PATH && dbt docs generate --profiles-dir={} && python3 -m http.server 8888 --directory target".format(
         ENV_VARIABLES["dbt_profiles_path"]
     )
     component["resource"] = {"cpus": 1, "memory": "512"}
+
+    env = {}
+    env["GIT_PROJECT_URL"] = ENV_VARIABLES["git_project_url"]
+    env["DBT_PROJECT_PATH"] = ENV_VARIABLES["dbt_project_path"] 
+
+    configuration = {}
+    configuration["env"] = env
+    component["configuration"] = configuration
 
     payload = {}
     payload["name"] = "dbt-service"
@@ -70,12 +79,14 @@ def generate_yarn_payload():
     return payload
 
 
-# host dbt docs on yan container
+# host dbt docs on yarn container
 def host_dbt_docs():
     load_fetch_environment_variables()
     payload = generate_yarn_payload()
     headers = {"Content-Type": "application/json"}
     gssapi_auth = HTTPSPNEGOAuth()
+
+    # Rest Api doc: https://hadoop.apache.org/docs/stable/hadoop-yarn/hadoop-yarn-site/yarn-service/YarnServiceAPI.html#ConfigFile
     response = requests.post(
         ENV_VARIABLES["yarn_rm_uri"] + "/app/v1/services",
         data=json.dumps(payload),
